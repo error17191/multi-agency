3ebbc5e6b4fb7d82e391c1cf628f3bd65ce68e94
Mohamed Ahmed <error17191@gmail.com>
2018-06-21 20:19:05 +0200

    Thinking about structure

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,24 +51,4 @@ class Handler extends ExceptionHandler
         return parent::render($request, $exception);
     }
 
-
-    /**
-     * Convert an authentication exception into a response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
-     * @return \Illuminate\Http\Response
-     */
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if(Agency::current()){
-            $routeLogin = route('agency.login');
-        }else{
-            $routeLogin = route('login');
-        }
-        return $request->expectsJson()
-            ? response()->json(['message' => $exception->getMessage()], 401)
-            : redirect()->guest($routeLogin);
-    }
-
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -76,7 +76,6 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware(['api', 'agency', 'bindings'])
             ->namespace($this->namespace)
-            ->name('agency.')
             ->domain('{agency}.' . config('app.domain'))
             ->group(base_path('routes/agencies/api.php'));
     }
@@ -85,7 +84,6 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware(['web', 'agency', 'bindings'])
             ->namespace($this->namespace)
-            ->name('agency.')
             ->domain('{agency}.' . config('app.domain'))
             ->group(base_path('routes/agencies/web.php'));
     }
